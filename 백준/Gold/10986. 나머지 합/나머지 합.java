8675051eaa6f00ef6db3937aae0c09aa3305c598
NaHyun Kim <80094320+knh4437@users.noreply.github.com>
2023-03-01 00:54:38 +0900

    [Gold III] Title: 나머지 합, Time: 500 ms, Memory: 120672 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 10986번 - 나머지 합 : https://www.acmicpc.net/problem/10986
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int count = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] S = new long[count];
+        long[] C = new long[M];
+        long ans = 0;
+        st = new StringTokenizer(bf.readLine());
+        S[0] = Integer.parseInt(st.nextToken());
+        for (int i=1;i<count;i++) {
+            // M으로 나머지 연산을 수행한 합 배열 S
+            S[i] = S[i-1] + Integer.parseInt(st.nextToken());
+        }
+        for (int i=0;i<count;i++) {
+            long reminder = S[i] % M;
+            if (reminder == 0)
+                ans++;
+            C[(int)reminder]++;
+        }
+        for (int i=0;i<M;i++) {
+            if (C[i] > 1)
+                ans+= C[i]*(C[i]-1)/2;
+        }
+        System.out.println(ans);
+    }
+}
