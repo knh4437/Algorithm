a0c703f1814d73878952dbbe712eddaf26d06b0d
NaHyun Kim <80094320+knh4437@users.noreply.github.com>
2023-04-07 17:43:30 +0900

    [Unrated] Title: 스택 수열, Time: 1024 ms, Memory: 112044 KB -BaekjoonHub

---
+++ 백준/Unrated/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+// 백준 1874번 - 스택 수열 : https://www.acmicpc.net/problem/1874
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        boolean IsNo = false;
+        for(int i=0;i<N;i++) {
+            A[i] = sc.nextInt();
+        }
+        StringBuffer sb = new StringBuffer();
+        int num=1;
+        for(int i=0;i<A.length;i++) {
+            if (A[i] >= num) {
+                while (A[i] >= num) {
+                    stack.push(num++);
+                    sb.append("+\n");
+                }
+                stack.pop();
+                sb.append("-\n");
+            }
+            else {
+                int top = stack.pop();
+                if (top > A[i]) {
+                    System.out.println("NO");
+                    IsNo = true;
+                    break;
+                }
+                else sb.append("-\n");
+            }
+        }
+        if (IsNo==false)
+            System.out.println(sb);
+    }
+}
