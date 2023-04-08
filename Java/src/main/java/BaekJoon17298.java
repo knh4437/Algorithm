6fe400f70cd30976b88acb4645a5adf69d80eacd
knh4437 <knh4437@naver.com>
2023-04-09 00:31:59 +0900

    Solved: BaekJoon17298

---
+++ Java/src/main/java/BaekJoon17298.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+// 백준 17298번 - 오큰수 : https://www.acmicpc.net/problem/17298
+public class BaekJoon17298 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int A[] = new int[N];
+        int result[] = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for(int i=0;i<N;i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for(int i=1;i<N;i++) {
+            int num = A[i];
+            while (!stack.isEmpty() && A[stack.peek()] < num) {
+                int top = stack.pop();
+                result[top] = num;
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()) {
+            int top = stack.pop();
+            result[top] = -1;
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i=0;i<N;i++) {
+            bw.write(result[i] + " ");
+        }
+        bw.close();
+    }
+}
