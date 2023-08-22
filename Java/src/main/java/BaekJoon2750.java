c550334f5ee88dc189215c18b508f5ecd5393d9c
knh4437 <knh4437@naver.com>
2023-08-22 23:10:03 +0900

    Solved: BaekJoon 2750

--- Java/src/main/java/BaekJoon2750.java
+++ Java/src/main/java/BaekJoon2750.java
@@ -8,11 +8,19 @@ public class BaekJoon2750 {
         int[] list = new int[N];
         for (int i=0;i<N;i++) {
             list[i] = sc.nextInt();
-            if (i > 0 && list[i] > list[i-1]) {
-                for (int j=i;j>0;j--) {
+        }
 
+        for(int i=0;i<N-1;i++) {
+            for (int j=0;j<N-1-i;j++) {
+                if (list[j] > list[j+1]) {
+                    int temp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = temp;
                 }
             }
         }
+
+        for(int i=0;i<N;i++)
+            System.out.println(list[i]);
     }
 }
