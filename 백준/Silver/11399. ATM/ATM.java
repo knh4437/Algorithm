36f29f0ffdc61f626fa2ef5cab201817d2d925e4
NaHyun Kim <80094320+knh4437@users.noreply.github.com>
2023-08-24 02:35:45 +0900

    [Silver IV] Title: ATM, Time: 288 ms, Memory: 20712 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+// 백준 11399 - ATM : https://www.acmicpc.net/problem/11399
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] list = new int[N];
+        int[] S = new int[N+1];
+        int sum = 0;
+        for (int i=0;i<N;i++) {
+            list[i] = sc.nextInt();
+        }
+        for (int i=1;i<N;i++) {
+            int key = list[i];
+            int j = i-1;
+            while (j>=0 && key < list[j]) {
+                list[j+1] = list[j];
+                j--;
+            }
+            list[j+1] = key;
+        }
+        S[0] = list[0];
+        for (int i=1;i<N;i++) {
+            S[i] = S[i-1] + list[i];
+        }
+        // 합 배열
+        for (int i=0;i<N;i++) {
+            sum += S[i];
+        }
+        System.out.println(sum);
+    }
+}
