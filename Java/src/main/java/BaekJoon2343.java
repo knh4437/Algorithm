f7c361d6711652da445d5a2206fb69166c289806
knh4437 <knh4437@naver.com>
2023-10-18 17:45:45 +0900

    Solved: BaekJoon2343

---
+++ Java/src/main/java/BaekJoon2343.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BaekJoon2343 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] list = new int[N];
+        int start=0;
+        int end=0;
+        for(int i=0;i<N;i++) {
+            list[i] = sc.nextInt();
+            if (start < list[i]) start = list[i];
+            end+=list[i];
+        }
+
+        while (start <= end) {
+            int mid = (start+end) / 2;
+            int sum=0;
+            int count=0;
+            for(int i=0;i<N;i++) {
+                if (sum + list[i] > mid) {
+                    count++;
+                    sum =0;
+                }
+                sum += list[i];
+            }
+            if (sum != 0)
+                count++;
+            if (count > M)
+                start = mid +1;
+            else
+                end = mid - 1;
+        }
+        System.out.println(start);
+    }
+}
