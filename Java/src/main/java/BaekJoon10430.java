0e5827bfcc038802ae6f15a01104adaf6e31971a
knh4437 <knh4437@naver.com>
2023-12-22 22:52:03 +0900

    Solved: BaekJoon step 1

---
+++ Java/src/main/java/BaekJoon10430.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BaekJoon10430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = Integer.parseInt(sc.next());
+        int B = Integer.parseInt(sc.next());
+        int C = Integer.parseInt(sc.next());
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C)+(B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
---
+++ Java/src/main/java/BaekJoon10926.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon10926 {
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String s = bf.readLine();
+        System.out.print(s + "??!");
+    }
+}
