5604f768f67f24083bcbacfabcd83060726f0c3f
NaHyun Kim <80094320+knh4437@users.noreply.github.com>
2023-12-22 22:11:30 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1556 ms, Memory: 57280 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        ArrayList<int[]> arrayList = new ArrayList<>();
+
+        for(int i=0;i<N;i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arrayList.add(new int[]{x, y});
+        }
+        Collections.sort(arrayList, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0])
+                    return o1[1] - o2[1];
+                else
+                    return o1[0] - o2[0];
+            }
+        });
+
+        for(int[] point : arrayList) {
+            System.out.println(point[0] + " " + point[1]);
+        }
+    }
+}
