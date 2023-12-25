eef6eeb119ad0114f2bc23350e9d9e1d047f9722
knh4437 <knh4437@naver.com>
2023-12-25 23:35:03 +0900

    Solved: BaekJoon 15688

--- Java/src/main/java/BaekJoon15688.java
+++ Java/src/main/java/BaekJoon15688.java
@@ -1,5 +1,22 @@
-public class BaekJoon15688 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Arrays;
 
+public class BaekJoon15688 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(bf.readLine());
+        int[] list = new int[N];
+        for(int i=0;i<N;i++)
+            list[i] = Integer.parseInt(bf.readLine());
+
+        Arrays.sort(list);
+
+        for(int i:list)
+            sb.append(i).append("\n");
+
+        System.out.print(sb);
     }
+
 }
