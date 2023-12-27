6ed0ccdac119bfc38f0939c71348bbbcaad68ec8
knh4437 <knh4437@naver.com>
2023-12-27 23:19:35 +0900

    Solved: BaekJoon 1676

---
+++ Java/src/main/java/BaekJoon1676.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int count =0;
+
+        while (N>=5) {
+            count += N/5;
+            N/=5;
+        }
+        System.out.println(count);
+    }
+}
