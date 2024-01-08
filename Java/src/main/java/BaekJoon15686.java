c3c9407b4222d25876d5f2ab4725ac062af4c2ed
knh4437 <knh4437@naver.com>
2024-01-08 18:20:38 +0900

    Solved: BaekJoon15649

---
+++ Java/src/main/java/BaekJoon15686.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+    2 <= N <= 50 : 도시의 가로, 세로 크기
+    1 <= M <= 13 : 가장 수익을 맣이 낼 수있는 치킨집의 개수
+
+*/
+public class BaekJoon15686 {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
