9b4d3407649754c53aab9d02d9ff698066e45ecc
knh4437 <knh4437@naver.com>
2024-03-29 01:17:45 +0900

    Solved: BaekJoon1931

--- Java/src/main/java/test.java
+++ Java/src/main/java/test.java
@@ -1,5 +1,13 @@
 public class test {
+
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println(Holder.Inner.num);
+    }
+
+}
+
+class Holder {
+    static class Inner {
+        static int num=1;
     }
 }
