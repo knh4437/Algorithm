e82c957bc359da56c6ea4b503f99d9ec6a012d4a
NaHyun Kim <80094320+knh4437@users.noreply.github.com>
2024-04-03 18:14:15 +0900

    [level 2] Title: 무인도 여행, Time: 0.40 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,15 +1,19 @@
 import java.util.*;
 
 class Solution {
-    static int N, M;
+    static boolean[][] visited;
     static int[][] board;
+    static int N,M;
+    static int[] mx = {0,0,1,-1};
+    static int[] my = {1,-1,0,0};
+    static ArrayList<Integer> list;
     
     public ArrayList<Integer> solution(String[] maps) {
         int[] answer = {};
         N = maps.length;
         M = maps[0].length();
         board = new int[N][M];
-        ArrayList<Integer> list = new ArrayList<>();
+        list = new ArrayList<>();
         
         for(int i=0;i<N;i++) {
             for(int j=0;j<M;j++) {
@@ -19,42 +23,38 @@ class Solution {
             }    
         }
         
+        int result = 0;
+        
         for(int i=0;i<N;i++) {
-            for(int j=0;j<M;j++) {
-                if (board[i][j] != -1)
-                    list.add(bfs(i,j));
+            for (int j=0;j<M;j++) {
+                if (board[i][j] != -1) {
+                    result = dfs(i,j, result);
+                    list.add(result);
+                    result = 0;
+                }
             }
         }
-        Collections.sort(list);
+        
         if (list.size() == 0)
             list.add(-1);
+        
+        Collections.sort(list);
+        
         return list;
     }
     
-    private static int bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x,y});
-        int sum = board[x][y];
-        board[x][y]=-1;
-        int[] mx = {0,0,1,-1};
-        int[] my = {1,-1,0,0};
-        
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            
-            for(int i=0;i<4;i++) {
-                int nx = cur[0] + mx[i];
-                int ny = cur[1] + my[i];
-                
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M)
-                    continue;
-                if (board[nx][ny] == -1)
-                    continue;
-                queue.add(new int[]{nx,ny});
-                sum += board[nx][ny];
-                board[nx][ny] = -1;
-            }
+    private static int dfs(int x, int y, int result) {
+        result += board[x][y];
+        board[x][y] = -1;
+        for(int i=0;i<4;i++) {
+            int nx = x + mx[i];
+            int ny = y + my[i];
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+                continue;
+            if (board[nx][ny]==-1)
+                continue;
+            result = dfs(nx,ny,result);
         }
-        return sum;
+        return result;
     }
 }
