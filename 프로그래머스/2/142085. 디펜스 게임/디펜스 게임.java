a8b4caeb96c1c42d5242de82531f12cd65f119b3
NaHyun Kim <80094320+knh4437@users.noreply.github.com>
2024-04-06 23:11:34 +0900

    [level 2] Title: 디펜스 게임, Time: 0.69 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = enemy.length;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i=0;i<enemy.length;i++) {
+            n -= enemy[i];
+            queue.add(enemy[i]);
+            if (n < 0) {
+                if (k > 0 && !queue.isEmpty()) {
+                    n += queue.poll();
+                    k--;
+                } else { 
+                    answer = i;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
