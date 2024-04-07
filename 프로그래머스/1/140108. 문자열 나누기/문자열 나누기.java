b81a1d94eb1b75c356e030146e8b0ce5ada5ade9
NaHyun Kim <80094320+knh4437@users.noreply.github.com>
2024-04-07 23:21:58 +0900

    [level 1] Title: 문자열 나누기, Time: 0.49 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -3,14 +3,14 @@ class Solution {
         int answer = 0;
         int same = 0;
         int diff = 0;
-        char ch = s.charAt(0);
+        char x = s.charAt(0);
         
         for(int i=0;i<s.length();i++) {
             if (same == diff) {
                 answer++;
-                ch = s.charAt(i);
+                x = s.charAt(i);
             }
-            if (s.charAt(i) == ch) same++;
+            if (s.charAt(i) == x) same++;
             else diff++;
         }
         
