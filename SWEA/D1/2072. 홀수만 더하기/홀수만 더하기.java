a5bb6b8b76f1f203e449da8d4db8764f1a2ef672
NaHyun Kim <80094320+knh4437@users.noreply.github.com>
2024-05-03 01:41:09 +0900

    [D1] Title: 홀수만 더하기, Time: 126 ms, Memory: 25,976 KB -BaekjoonHub

---
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            long sum = 0;
+            for(int i=0;i<10;i++) {
+                int num = sc.nextInt();
+                if (num%2!=0)
+                    sum += num;
+            }
+            bw.write("#"+test_case + " "+sum+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
